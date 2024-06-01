c390df99eccece89f4b58355bf5d6290c55992a8
Hashino <iam.ravara@gmail.com>
2024-06-01 15:14:17 -0300

    basic image rendering done
    still synchronous. may god help me making this become asynchronous

--- src/ui/popup/popup.rs
+++ src/ui/popup/popup.rs
@@ -15,7 +15,7 @@ use std::io::{Error, Result};
 
 use crossterm::event::{self, KeyCode};
 use ratatui::{
-    layout::{Alignment, Constraint, Direction, Layout, Rect},
+    layout::Alignment,
     style::{Color, Modifier, Style, Stylize},
     widgets::{
         block::{self, Position},
@@ -23,7 +23,7 @@ use ratatui::{
     },
 };
 
-use crate::tui;
+use crate::{tui, ui::utils::Utils};
 
 pub static KEYBINDINGS_INFO: &str = "[?]         - Show this window
 [j/k]       - Navigate up/down in list
@@ -67,7 +67,7 @@ pub fn show_info(title: &str, info: String) {
             let info_size = info.clone().chars().filter(|c| *c == '\n').count();
             let paragraph = Paragraph::new(info.clone()).wrap(Wrap { trim: false }); //message body
 
-            let inner = block.inner(centered_rect(65, 5 + info_size as u16, frame.size()));
+            let inner = block.inner(Utils::centered_rect(65, 5 + info_size as u16, frame.size()));
 
             frame.render_widget(paragraph.clone().block(block), inner);
         })?;
@@ -125,7 +125,7 @@ pub fn show_confirmation(title: &str, info: String) -> bool {
             let paragraph = Paragraph::new(info.clone()).style(Style::default().fg(Color::Cyan));
             let info_size = info.clone().chars().filter(|c| *c == '\n').count();
 
-            let inner = block.inner(centered_rect(25, 5 + info_size as u16, frame.size()));
+            let inner = block.inner(Utils::centered_rect(25, 5 + info_size as u16, frame.size()));
 
             frame.render_widget(paragraph.clone().block(block), inner);
         })?;
@@ -151,28 +151,4 @@ pub fn show_confirmation(title: &str, info: String) -> bool {
             false
         }
     }
-}
-/// helper function to create a centered rect
-/// * [width](u16) in characters of text
-/// * [height](u16) in lines of text
-fn centered_rect(width: u16, height: u16, r: Rect) -> Rect {
-    // Cut the given rectangle into three vertical pieces
-    let popup_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([
-            Constraint::Length((r.height - height) / 2),
-            Constraint::Length(height),
-            Constraint::Length((r.height - height) / 2),
-        ])
-        .split(r);
-
-    // Then cut the middle vertical piece into three width-wise pieces
-    Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Length((r.width - width) / 2),
-            Constraint::Length(width),
-            Constraint::Length((r.width - width) / 2),
-        ])
-        .split(popup_layout[1])[1] // Return the middle chunk
 }
---
+++ src/ui/preview_pane/preview_pane.rs
@@ -0,0 +1,99 @@
+use crossterm::event::KeyEvent;
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    text::Line,
+    widgets::{Paragraph, Widget},
+};
+use std::path::PathBuf;
+
+use crate::ui::{
+    file_picker::{dir::Dir, file_picker::FilePicker},
+    utils::Utils,
+};
+
+pub struct PreviewPane {
+    curr_entry: PathBuf,
+    pub active: bool,
+    pub needs_redraw: bool,
+    file_picker: FilePicker,
+}
+
+impl PreviewPane {
+    pub fn new() -> PreviewPane {
+        PreviewPane {
+            curr_entry: PathBuf::new(),
+            active: false,
+            needs_redraw: false,
+            file_picker: FilePicker::new(false),
+        }
+    }
+    pub fn initialize(&mut self, dir: Option<PathBuf>) {
+        match dir.clone() {
+            Some(value) => {
+                self.curr_entry = value;
+                if self.curr_entry.is_dir() {
+                    self.file_picker.initialize(dir, Some(0));
+                }
+            }
+            None => self.curr_entry = Dir::get_cur_dir().pathbuf,
+        }
+    }
+    pub fn handle_keys(&mut self, key: KeyEvent) {
+        if self.curr_entry.is_dir() {
+            self.file_picker.handle_keys(key);
+        }
+    }
+    pub fn render(&mut self, area: Rect, buf: &mut Buffer) {
+        // if its a directory shows another file picker
+        if self.curr_entry.is_dir() {
+            self.file_picker.render(area, buf);
+        } else if self.curr_entry.is_file() {
+            match self.curr_entry.extension() {
+                Some(extension) => match extension.to_str() {
+                    Some("png") | Some("jpg") | Some("jpeg") => {
+                        get_image_ascii_paragraph(area, buf, self.curr_entry.clone());
+                    }
+                    _ => {
+                        let text = Paragraph::new("Not yet implementd");
+                        text.render(area, buf)
+                    }
+                },
+                None => (),
+            }
+        }
+    }
+}
+use image::DynamicImage;
+use image::{imageops::FilterType, io::Reader as ImageReader};
+use image_ascii::TextGenerator;
+
+// TODO: find a library to convert images to ASCII
+fn get_image_ascii_paragraph(area: Rect, buf: &mut Buffer, image_file: PathBuf) {
+    let drawing_characters = &[' ', '🞌', '🞍', '🞐', '🞑', '🞒', '🞓', '🞕'];
+
+    let dynamic_image: DynamicImage = ImageReader::open(image_file).unwrap().decode().unwrap();
+
+    let fixed_widht_image = dynamic_image.resize_exact(
+        (dynamic_image.width() as f32 * 2.5) as u32,
+        dynamic_image.height(),
+        FilterType::Nearest,
+    );
+
+    let resized_image = fixed_widht_image.thumbnail(area.width.into(), area.height.into());
+
+    let ascii: String = TextGenerator::new(&resized_image)
+        .set_density_chars(drawing_characters)
+        .unwrap()
+        .generate();
+
+    let result: Vec<Line> = ascii.lines().map(|line| Line::raw(line)).collect();
+
+    let centered_area = Utils::centered_rect(
+        result[0].width().try_into().unwrap_or_default(),
+        result.len().try_into().unwrap_or_default(),
+        area,
+    );
+
+    Paragraph::new(result).render(centered_area, buf)
+}
